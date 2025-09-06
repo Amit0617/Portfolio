5e9d6fa5d027d11125aa78b3e552103328720045
Amit0617 <71893015+Amit0617@users.noreply.github.com>
2025-09-06 21:35:46 +0530

    copy dist to /tmp

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  base: '/Portfolio/',
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
